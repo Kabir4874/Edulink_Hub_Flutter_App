f06b0ef97c6877730d0e8b2ad34366174d5484df
kabir4874 <kabir.cse.bd@gmail.com>
2025-04-04 22:11:48 +0600

    university backend added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+
+import 'screens/admission_page.dart'; // Admission page import
+import 'screens/books_page.dart'; // Books page import
+import 'screens/forgot_password_page.dart'; // Forgot password page
+import 'screens/funding_page.dart'; // Funding page import
+import 'screens/home_page.dart'; // Home page import
 import 'screens/sign_in_page.dart'; // Sign-in page import
 import 'screens/sign_up_page.dart'; // Sign-up page import
-import 'screens/forgot_password_page.dart'; // Forgot password page
-import 'screens/home_page.dart'; // Home page import
-import 'screens/admission_page.dart'; // Admission page import
-import 'screens/funding_page.dart'; // Funding page import
-import 'screens/books_page.dart'; // Books page import
 import 'screens/subscription_page.dart';
 // import 'screens/Admin/admin_sign_in_page.dart'; // Admin sign-in
 // import 'screens/Admin/admin_dashboard_page.dart';
@@ -32,7 +33,8 @@ class EdulearnApp extends StatelessWidget {
         '/signup': (context) => SignUpPage(),
         '/forgotPassword': (context) => ForgotPasswordPage(),
         '/home': (context) => HomePage(), // Add HomePage route
-        '/admission': (context) => AdmissionPage(), // Placeholder for Admission Page
+        '/admission': (context) =>
+            AdmissionPage(), // Placeholder for Admission Page
         '/funding': (context) => FundingPage(), // Placeholder for Funding Page
         '/books': (context) => BooksPage(), // Placeholder for Books Page
         '/subscription': (context) => SubscriptionPage(),
@@ -41,18 +43,7 @@ class EdulearnApp extends StatelessWidget {
         // '/admin-admission-management': (context) => AdminAdmissionManagementPage(),
         // '/admin-funding-management': (context) => AdminFundingManagementPage(),
         // '/admin-books-management': (context) => AdminBooksManagementPage(),
-
       },
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
